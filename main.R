929524546c07c3baa5f5b258add04c599d5c55d7
agouy <alexandre.gouy@gmail.com>
2020-05-15 15:10:23 +0000

    added try()

--- main.R
+++ main.R
@@ -2,16 +2,26 @@ library(tercen)
 library(dplyr)
 
 do.lm <- function(df) {
-  mod <- lm(.y ~ .x, data = df)
+  
   out <- data.frame(
     .ri = df$.ri[1],
     .ci = df$.ci[1],
-    intercept = mod$coefficients[1],
-    slope = mod$coefficients[2],
-    fit.y = range(mod$fitted.values), # Get min and max predictions
-    fit.x = range(df$.x) # Get min and max x
+    intercept = NaN,
+    slope = NaN,
+    fit.y = rep(NaN, 2),
+    fit.x = rep(NaN, 2)
   )
+  
+  mod <- try(lm(.y ~ .x, data = df))
+  
+  if(!inherits(mod, 'try-error')) {
+    out$intercept <- mod$coefficients[1]
+    out$slope <- mod$coefficients[2]
+    out$fit.y <- range(mod$fitted.values)
+    out$fit.x <- range(df$.x)
+  }
   return(out)
+  
 }
 
 (ctx <- tercenCtx())  %>% 
