c3d3ea1242227abe3792100e4a53d4fa6b50bd04
Alexandre Gouy <contact@agouy.com>
2020-10-17 20:16:19 +0200

    Update main.R

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 library(tercen)
 library(dplyr)
 
-do.lm <- function(df) {
+do.lm <- function(df, intercept.omit) {
   
   out <- data.frame(
     .ri = df$.ri[1],
@@ -12,13 +12,11 @@ do.lm <- function(df) {
     #fit.x = rep(NaN, 2)
   )
   
-  intercept.omit <- as.logical(ctx$op.value('intercept.omit'))
   if(intercept.omit) {
     mod <- try(lm(.y ~ .x - 1, data = df))
   } else {
     mod <- try(lm(.y ~ .x, data = df))
   }
-  
   
   if(!inherits(mod, 'try-error')) {
     out$intercept <- mod$coefficients[1]
@@ -27,7 +25,6 @@ do.lm <- function(df) {
     #out$fit.x <- range(df$.x)
   }
   return(out)
-  
 }
 
 ctx <- tercenCtx()
@@ -35,9 +32,11 @@ ctx <- tercenCtx()
 if(inherits(try(ctx$select(".x")), 'try-error')) stop("x axis is missing.")
 if(inherits(try(ctx$select(".y")), 'try-error')) stop("y axis is missing.")
 
+intercept.omit <- as.logical(ctx$op.value('intercept.omit'))
+
 ctx %>% 
   select(.x, .y, .ri, .ci) %>% 
   group_by(.ri, .ci) %>%
-  do(do.lm(.)) %>%
+  do(do.lm(., intercept.omit)) %>%
   ctx$addNamespace() %>%
   ctx$save()
