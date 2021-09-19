62a886284f69379b1e22127e95d712008176a402
agouy <contact@agouy.com>
2021-09-19 14:49:52 +0000

    replaced test

--- workspace.R
+++ workspace.R
@@ -1,39 +1,36 @@
 library(tercen)
 library(dplyr)
 
-options("tercen.workflowId" = "f81d245ef22a2ff192ed2533a6002ec3")
-options("tercen.stepId"     = "5048c052-db65-475b-a9ac-97733e2b8d0d")
+options("tercen.workflowId" = "5c90e02366f9396ae469cb7feb004464")
+options("tercen.stepId"     = "4be0fb98-4211-40ca-a79c-9b13b1826762")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-do.lm <- function(df) {
+do.lm <- function(df, intercept.omit) {
   
   out <- data.frame(
     .ri = df$.ri[1],
     .ci = df$.ci[1],
     intercept = NaN,
-    slope = NaN,
-    fit.y = rep(NaN, 2),
-    fit.x = rep(NaN, 2)
+    slope = NaN#,
+    #fit.y = rep(NaN, 2),
+    #fit.x = rep(NaN, 2)
   )
   
-  intercept.omit <- as.logical(ctx$op.value('intercept.omit'))
   if(intercept.omit) {
     mod <- try(lm(.y ~ .x - 1, data = df))
   } else {
     mod <- try(lm(.y ~ .x, data = df))
   }
   
-  
   if(!inherits(mod, 'try-error')) {
     out$intercept <- mod$coefficients[1]
     out$slope <- mod$coefficients[2]
-    out$fit.y <- range(mod$fitted.values)
-    out$fit.x <- range(df$.x)
+    #out$fit.y <- range(mod$fitted.values)
+    #out$fit.x <- range(df$.x)
   }
   return(out)
-  
 }
 
 ctx <- tercenCtx()
@@ -41,9 +38,12 @@ ctx <- tercenCtx()
 if(inherits(try(ctx$select(".x")), 'try-error')) stop("x axis is missing.")
 if(inherits(try(ctx$select(".y")), 'try-error')) stop("y axis is missing.")
 
-ctx %>%
+intercept.omit <- FALSE
+intercept.omit <- as.logical(ctx$op.value('intercept.omit'))
+
+ctx %>% 
   select(.x, .y, .ri, .ci) %>% 
   group_by(.ri, .ci) %>%
-  do(do.lm(.)) %>%
+  do(do.lm(., intercept.omit)) %>%
   ctx$addNamespace() %>%
   ctx$save()
